fca798af02debe77c9bc30fad8fa1556f09e84b7
Melman Dmitriy <melman.dmitriy@wb.ru>
2024-12-04 18:22:08 +0300

    add: небольшие правки формы

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ function App() {
   return (
     <div>
       <h1>Frontend Advent</h1>
-      <a href="https://simple-on-it.com/frontend-advent/calendar?date=2024-12-01">
+      <a
+        href="https://simple-on-it.com/frontend-advent/calendar?date=2024-12-01"
+        target="_blank"
+      >
         Материалы Эвента
       </a>
 
       <div className="tasks-list">
         {tasks.map((task: Task) => (
           <div className="task">
-            <p>{task.name}</p>
-            <p>{task.day}</p>
+            <p className="task__name">{task.name}</p>
+            <p className="task__day">день: {task.day}</p>
+            <p>{task.discription}</p>
             <a href={task.url} target="_blank">
               реализация
             </a>
--- src/model/controller.ts
+++ src/model/controller.ts
@@ -1,5 +1,6 @@
 export interface Task {
   name: string;
+  discription?: string;
   day: string;
   url?: string;
   video?: string;
@@ -7,8 +8,9 @@ export interface Task {
 
 export const tasks: Task[] = [
   {
-    name: "HTML form",
+    name: "HTML",
     day: "1",
+    discription: "форма",
     url: "https://space-and-software.github.io/FA-day-1/",
     video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
   },
@@ -16,5 +18,41 @@ export const tasks: Task[] = [
     name: "day 2",
     day: "2",
     url: "",
+  },
+  {
+    name: "HTML form",
+    day: "1",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
+  },
+  {
+    name: "Responsive design",
+    day: "3",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
+  },
+  {
+    name: "HTML form",
+    day: "1",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
+  },
+  {
+    name: "HTML form",
+    day: "1",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
+  },
+  {
+    name: "HTML form",
+    day: "1",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
+  },
+  {
+    name: "HTML form",
+    day: "1",
+    url: "https://space-and-software.github.io/FA-day-1/",
+    video: "https://www.youtube.com/shorts/96H45Uf0_hQ",
   },
 ];
